2fd14457991ae32d79a4f3040684ce2c6e5157d2
Seymur Umudov <seymur.umudov@code.local>
2024-03-30 18:18:25 +0400

    biz ilk sectionu tamamladiq

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".mySlider", {
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+});
